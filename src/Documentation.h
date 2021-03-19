5381b528bcc1811ccfbfbf76351251e1a189472d
GRILLON THEO p1907354 <theo.grillon@etu.univ-lyon1.fr>
2021-03-19 14:53:38 +0000

    Update Documentation.h

---
+++ src/Documentation.h
@@ -0,0 +1,24 @@
+/** \mainpage Nibble
+
+\section Introduction 
+Projet réalisé dans le cadre de l'UE LIFAP4 en 2e année de licence Informatique à l'Université Claude Bernard Lyon 1.
+
+\section Pour compiler 
+tester sous Linux(Ubuntu) et Windows.
+
+Dépendances:
+    \n SDL : https://www.libsdl.org/
+
+\n $ make ou ouvrir le projet avec CodeBlocks puis F9
+
+\section Pour éxecuter 
+
+\n $ cd .../nibble_p1803192_p1907354_p1801934
+
+\section Pour générer la documentation de code
+
+\n Dépendance : Doxygen http://www.stack.nl/~dimitri/doxygen/ 
+\n $ cd ...module-image/doc
+\n Puis ouvrir ...module-image/doc/html/index.html avec firefox
+
+*/
