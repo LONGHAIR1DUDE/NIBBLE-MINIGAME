28ca82c74975746e3d8728886c87342cc561815a
theo.grillon <theo.grillon@etu.univ-lyon1.fr>
2021-03-21 15:12:39 +0100

    ajout src/TxtFenetre.*

---
+++ src/TxtFenetre.h
@@ -0,0 +1,20 @@
+#ifndef _TXTFENETRE_H
+#define _TXTFENETRE_H
+
+class TxtFenetre {
+private:
+    int dimx, dimy;
+    char* fenetre;
+public:
+    TxtFenetre(int dx, int dy);
+
+    void effacer();
+    void ecrire(int x, int y, char c);
+    void ecrire(int x, int y, char* c);
+    void dessiner(int x, int y);
+    void pause();
+    char getCh();
+};
+
+
+#endif
