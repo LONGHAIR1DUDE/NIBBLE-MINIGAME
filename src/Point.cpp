35f6076da7139cad2818ca6600e02dc15452123d
long-hair-dude <p1803192@forge.univ-lyon1.fr>
2021-03-23 12:32:34 +0100

    Maj Point.*

---
+++ src/Point.cpp
@@ -0,0 +1,17 @@
+#include "Point.h"
+using namespace std;
+
+
+Point::Point () {}
+
+Point::Point (int valX, int valY) {
+    x = valX;
+    y = valY; 
+}
+
+Point& Point::operator =(const Point& operandeDroite){
+    x = operandeDroite.x;
+    y = operandeDroite.y;
+
+    return *this;
+}
---
+++ src/Point.h
@@ -0,0 +1,16 @@
+#ifndef _POINT_H
+#define _POINT_H
+#include<iostream>
+using namespace std;
+
+class Point {
+public:
+    int x;
+    int y;
+    Point();
+    Point(int valX, int valY);
+    Point& operator =(const Point & operandeDroite);
+
+};
+
+#endif
