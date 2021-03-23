312e83b7753b2613466b48be095b1c0797cafc79
long-hair-dude <p1803192@forge.univ-lyon1.fr>
2021-03-23 12:25:27 +0100

    Maj src/Jeu.*

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,8 +1,16 @@
 #include "Point.h"
 using namespace std;
 
-Point& Point::operator =(operandeDroite){
-    x=operandeDroite.x;
-    y=operandeDroite.y;
+Point::Point () {}
+
+Point::Point (int valX, int valY) {
+    x = valX;
+    y = valY; 
+}
+
+Point& Point::operator =(const Point& operandeDroite){
+    x = operandeDroite.x;
+    y = operandeDroite.y;
+
     return *this;
 }
