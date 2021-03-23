a0f4b0a7a2b39395ec588f2c01c11ad9e6f5b943
theo.grillon <theo.grillon@etu.univ-lyon1.fr>
2021-03-23 08:30:43 +0100

    ajout de mainAffiche.cpp

---
+++ src/mainAffiche.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Terrain.h"
+using namespace std;
+
+int main () {
+    Terrain terrain;
+    // terrain.recupNiveau("./data/niveau2.txt");
+    terrain.posAleaCle();
+    int tailleX = terrain.getDimX();
+    int tailleY = terrain.getDimY();
+    int tailleTerrain = terrain.getTailleTerrain();
+
+    cout << "size(): " << tailleTerrain << " " << "dimx: " << tailleX << " " << "dimy: " << tailleY << endl;
+    for (int y = 0; y < tailleY; y++) {
+        for (int x = 0; x < tailleX; x++) {
+            cout << terrain.getXY(x,y);
+        }
+        cout << endl;
+    }
+ 
+    return 0;
+}
