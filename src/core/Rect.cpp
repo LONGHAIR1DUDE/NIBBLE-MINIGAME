07f4ea73bfc0b6e8e79d8abb853da3bba7ed2452
theo.grillon <theo.grillon@etu.univ-lyon1.fr>
2021-04-03 19:03:38 +0200

    src/core/Rect.*

---
+++ src/core/Rect.cpp
@@ -0,0 +1,14 @@
+#include "Rect.h"
+#include <iostream>
+using namespace std;
+
+Rect::Rect () {}
+
+Rect::Rect(int cx, int cy, int dw, int dh) {
+    x = cx;
+    y = cy;
+    w = dw;
+    h = dh;
+}
+
+Rect::~Rect () {}
